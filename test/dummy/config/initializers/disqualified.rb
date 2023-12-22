812322c65936aa9f82462960599e5da1a9acfcec
Zach Ahn <engineering@zachahn.com>
2023-12-22 22:34:11 +0100

    Rename configuration options

--- test/dummy/config/initializers/disqualified.rb
+++ test/dummy/config/initializers/disqualified.rb
@@ -2,7 +2,7 @@ Disqualified.configure do |config|
   config.logger = Rails.logger
   ActiveRecord::Base.logger = Rails.logger
 
-  config.on_error do |error, context|
+  config.on_execution_error do |error, context|
     puts "🔥" * 10
     pp error
     pp context
