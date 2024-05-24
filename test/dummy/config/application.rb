3e1e49a1023bc3a9c880cfa9eebc7a008211885c
Zach Ahn <engineering@zachahn.com>
2024-05-24 10:28:15 -0400

    `rails app:update`

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -27,6 +27,11 @@ module Dummy
     # For compatibility with applications that use this config
     config.action_controller.include_all_helpers = false
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
