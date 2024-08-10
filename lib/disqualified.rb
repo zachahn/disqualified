dac265576cf429e975eb347808987246229ffa16
Zach Ahn <engineering@zachahn.com>
2024-08-10 11:10:32 -0400

    Refactor internal file structure of configuration

--- lib/disqualified.rb
+++ lib/disqualified.rb
@@ -12,9 +12,11 @@ require "sorbet-runtime"
 require_relative "disqualified/error"
 require_relative "disqualified/logging"
 
+require_relative "disqualified/configuration_structs"
+
+require_relative "disqualified/configuration"
 require_relative "disqualified/engine"
 require_relative "disqualified/job"
 require_relative "disqualified/main"
 require_relative "disqualified/pool"
-require_relative "disqualified/server_configuration"
 require_relative "disqualified/version"
--- lib/disqualified/configuration_structs.rb
+++ lib/disqualified/configuration_structs.rb
@@ -1,23 +1,5 @@
 # typed: strict
 
-module Disqualified
-  extend T::Sig
-
-  class << self
-    extend T::Sig
-
-    sig { returns(T.nilable(Disqualified::ServerConfiguration)) }
-    attr_accessor :server_options
-
-    sig { params(block: T.proc.params(arg0: Disqualified::ServerConfiguration).void).void }
-    def configure_server(&block)
-      if server_options
-        block.call(T.must(server_options))
-      end
-    end
-  end
-end
-
 class Disqualified::ServerConfiguration
   extend T::Sig
 
