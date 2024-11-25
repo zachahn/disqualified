c94bdbb9c5d9dcc916c34037b48df009de1a33fd
Zach Ahn <engineering@zachahn.com>
2024-11-24 23:38:43 -0500

    Support ActiveRecord's multi-database feature

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 * Added `Disqualified::Sequence`
+* Handle ActiveRecord's multi-database feature in the installation generator
 
 ### Changed
 
--- lib/generators/disqualified/install/install_generator.rb
+++ lib/generators/disqualified/install/install_generator.rb
@@ -1,11 +1,20 @@
+require "rails/generators/active_record"
+
 class Disqualified::InstallGenerator < Rails::Generators::Base
+  include ActiveRecord::Generators::Migration
+
   source_root File.expand_path("templates", __dir__)
+
+  class_option :database,
+    type: :string,
+    aliases: %i[--db],
+    desc: "The database for your migration. By default, the current environment's primary database is used."
 
   def copy_migration_file
     basename = "20220703062536_create_disqualified_jobs.rb"
-    copy_file(basename, "db/migrate/#{basename}")
+    copy_file(basename, File.join(db_migrate_path, basename))
 
     basename = "20241119023328_create_disqualified_sequences.rb"
-    copy_file(basename, "db/migrate/#{basename}")
+    copy_file(basename, File.join(db_migrate_path, basename))
   end
 end
